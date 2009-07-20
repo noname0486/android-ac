e1b9937aa4d882918c534c96bdb12909bc87452a
Dieter.Roth.Bayern <Dieter.Roth.Bayern@01c98a06-6bfe-11de-8922-5d95fdf06057>
2009-07-20 20:45:23 +0000

    GPL fileheaders

--- src/de/rothbayern/android/ac/pref/ColorPickerView.java
+++ src/de/rothbayern/android/ac/pref/ColorPickerView.java
@@ -1,40 +1,21 @@
 /*
- * Copyright (C) 2009 Analog Compass
- * 
- * This file is part of Analog Compass 
- * 
- * Analog Compass is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This source code is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this source code; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  "Analog Compass" is an application for devices based on android os. 
+ *  The application shows the orientation based on the intern magnetic sensor.   
+ *  Copyright (C) 2009  Dieter Roth
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms of the 
+ *  GNU General Public License as published by the Free Software Foundation; either version 3 of 
+ *  the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License along with this program; 
+ *  if not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.rothbayern.android.ac.pref;
-
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 
 import android.content.Context;
