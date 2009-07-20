26bd855d732c9e4ca462611e00afae30108f5857
Dieter.Roth.Bayern <Dieter.Roth.Bayern@01c98a06-6bfe-11de-8922-5d95fdf06057>
2009-07-20 20:12:07 +0000

    GPL fileheaders

--- src/de/rothbayern/android/ac/ACActivity.java
+++ src/de/rothbayern/android/ac/ACActivity.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2009 The Analog Compass
+ * Copyright (C) 2009 Analog Compass
  * 
  * This file is part of Analog Compass 
  * 
- * Analalog Compass is free software; you can redistribute it and/or modify
+ * Analog Compass is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
@@ -17,6 +17,7 @@
  * along with this source code; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 
 
 package de.rothbayern.android.ac;
--- src/de/rothbayern/android/ac/CalibrationActivity.java
+++ src/de/rothbayern/android/ac/CalibrationActivity.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2009 The Analog Compass
+ * Copyright (C) 2009 Analog Compass
  * 
  * This file is part of Analog Compass 
  * 
- * Analalog Compass is free software; you can redistribute it and/or modify
+ * Analog Compass is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
